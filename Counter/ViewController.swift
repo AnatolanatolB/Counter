9933b5cd009b024cfd0dc873d6b2089e88c2d63f
Анатолий Будников <anatolijbudnikov@iMac-Anatolij.local>
2025-06-04 13:13:38 +0700

    Правки

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -24,23 +24,24 @@ class ViewController: UIViewController {
     @IBAction func incrementCounter(_ sender: Any) {
         counter += 1
         counterLabel.text = "\(counter)"
-        changeView.text += "\n\(Date()) счетчик увеличен на 1"
+        changeView.text += "\n\(Date().formatted(date: .long, time: .standard)): счетчик увеличен на 1"
     }
+
     
     @IBAction func decreaseCounter(_ sender: Any) {
         if counter > 0 {
             counter -= 1
             counterLabel.text = "\(counter)"
-            changeView.text += "\n\(Date()) счетчик уменьшен на 1"
+            changeView.text += "\n\(Date().formatted(date: .long, time: .standard)): счетчик уменьшен на 1"
         } else {
-            changeView.text += "\n\(Date()) попытка уменьшить значение ниже 0"
+            changeView.text += "\n\(Date()): попытка уменьшить значение ниже 0"
         }
     }
     
     @IBAction func refreshCounter(_ sender: Any) {
         counter = 0
         counterLabel.text = "\(counter)"
-        changeView.text += "\n\(Date()): значаение сброшено"
+        changeView.text += "\n\(Date().formatted(date: .long, time: .standard)): значаение сброшено"
     }
     
     
